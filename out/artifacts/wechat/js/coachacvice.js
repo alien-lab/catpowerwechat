002536b9976a3be0fef75528e93ea3dc0f2e152e
xiaoleye <1419420280@qq.com>
2017-07-19 16:40:12 +0800

    整合钱都啊记录和其他

--- out/artifacts/wechat/js/coachacvice.js
+++ out/artifacts/wechat/js/coachacvice.js
@@ -6,17 +6,35 @@
     var app=angular.module("alienlab");
     app.controller("coachadviceController",["$scope","$stateParams","coachadviceService",function($scope,$stateParams,coachadviceService){
         console.log($stateParams.courseScheId);
-        $scope.coachadvice = coachadviceService.loadCoachAdvice();
+        var courseScheId = $stateParams.courseScheId;
+        coachadviceService.loadCoachAdvice(2,courseScheId,function (data) {
+            $scope.coachadvice = data
+        });
+        /*$scope.coachadvice =
+            coachadviceService.loadCoachAdvice();*/
     }]);
-    app.service("coachadviceService",[function () {
-        this.loadCoachAdvice = function () {
-            var coachadvice =
+    app.service("coachadviceService",["$http","domain",function ($http,domain) {
+        this.loadCoachAdvice = function (learnerId,courseScheId,callback) {
+            $http({
+                method:'POST',
+                url:domain+'api/learner-infos/coachadvice',
+                data:{
+                    learnerId:learnerId,
+                    courseScheId:courseScheId
+                }
+            }).then(function (data) {
+                if (callback) {
+                    callback(data.data);
+                }
+            })
+
+            /*var coachadvice =
                 {
                     "courseId":"1",
                     "eatInformation": "hha一定要多吃肉多吃肉多吃肉，恩要多吃肉，多吃肉，多吃甜食，多喝碳酸饮料，多睡觉，恩，就这样",
                     "fitInformation": "hhah没事多去跑步多去运动，多走路，多看风景，多看路，多种树，多造福人民，恩，就这样。"
                 };
-            return coachadvice;
+            return coachadvice;*/
         }
     }])
 
