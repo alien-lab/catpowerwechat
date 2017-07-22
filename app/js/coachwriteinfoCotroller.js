b40cd04121bc045cf29a13a228aa2f28762eadfa
AudreyDan <251918489@qq.com>
2017-07-22 15:06:40 +0800

    新增教练对学员健身反馈界面和js

---
+++ app/js/coachwriteinfoCotroller.js
@@ -0,0 +1,44 @@
+/**
+ * Created by ASUS on 2017/7/22.
+ */
+(function () {
+    'user strict';
+    var app = angular.module("alienlab");
+    app.controller("coachwriteinfoController",["$scope","coachwriteinfoService",function ($scope,coachwriteinfoService) {
+        $scope.submit=function (foodAdvice,exerciseAdvice) {
+            var coachAdvice={
+              "foodAdvice":foodAdvice,
+              "exerciseAdvice":exerciseAdvice
+            };
+            coachwriteinfoService.insertLearnerInfo(coachAdvice,2,3,function (data) {
+                swal({
+                    title:"您填写的学员健身反馈已提交",
+                    type:"success",
+                    showConfirmButton:false,
+                    timer:2000
+                });
+            })
+          
+        }
+    }]);
+
+    app.service("coachwriteinfoService",["$http","domain",function ($http,domain) {
+        this.insertLearnerInfo=function (coachAdvice,learnerId,scheId,callback) {
+            $http({
+                method:"POST",
+                url:domain+'api/learner-infos/fitlog',
+                data:{
+                    coachAdvice:coachAdvice,
+                    learnerId:learnerId,
+                    scheId:scheId
+                }
+            }).then(function (data) {
+                if (callback){
+                    callback(data.data);
+                }
+            })
+        }
+
+    }])
+
+})();
