41941e3546b3223735dd6be9bb4ae48fefbcd9ab
xiaoleye <1419420280@qq.com>
2017-07-25 16:51:16 +0800

    修改我的课程中的学院信息

--- app/js/stucourseController.js
+++ app/js/stucourseController.js
@@ -4,11 +4,31 @@
 (function () {
     'use strict';
     var app = angular.module("alienlab");
-    app.controller("stucourseController", ["$scope", "stuCourseService", "$rootScope", function ($scope, stuCourseService,$rootScope) {
-        var learnerId = $rootScope.learnerInfo.learner.id;
-        stuCourseService.loadStuCourse(learnerId, function (data) {
-            $scope.stuCourse = data;
-        })
+    app.controller("stucourseController", ["$scope", "stuCourseService", "$rootScope","stuindexService", function ($scope, stuCourseService,$rootScope,stuindexService) {
+        function loadLearner() {
+            if($rootScope.learnerInfo){
+                var learnerId = $rootScope.learnerInfo.learner.id;
+                if (learnerId){
+                    stuCourseService.loadStuCourse(learnerId, function (data) {
+                        $scope.stuCourse = data;
+                    })
+                }
+
+            }
+        }
+
+        $scope.$watch("$root.openid",function(newvalue){
+            if(!newvalue) return;
+            if(!$rootScope.learnerInfo){
+                stuindexService.loadStuIndex($rootScope.openid,function (data) {
+                    $scope.learnerIndex=data;
+                    $rootScope.learnerInfo = data;
+                    loadLearner();
+                });
+            }
+
+        },true)
+
     }]);
 
     app.service("stuCourseService", ["$http", "domain", function ($http, domain) {
