76f21977239eae45024d57a018e80abff798e1f1
xiaoleye <1419420280@qq.com>
2017-09-13 15:14:58 +0800

    教练端修改内容

--- app/js/coachwriteinfoCotroller.js
+++ app/js/coachwriteinfoCotroller.js
@@ -4,8 +4,8 @@
 (function () {
     'user strict';
     var app = angular.module("alienlab");
-    app.controller("coachwriteinfoController",["$scope","coachwriteinfoService","$stateParams",
-        function ($scope,coachwriteinfoService,$stateParams) {
+    app.controller("coachwriteinfoController",["$scope","coachwriteinfoService","$stateParams","$state",
+        function ($scope,coachwriteinfoService,$stateParams,$state) {
         $scope.submit=function (foodAdvice,exerciseAdvice) {
             console.log(foodAdvice,exerciseAdvice)
             var coachAdvice={
@@ -15,12 +15,15 @@
             var learnerId = $stateParams.learnerId;
             var scheId = $stateParams.scheId;
 
+
             coachwriteinfoService.insertLearnerInfo(coachAdvice,learnerId,scheId,function (data) {
                 swal({
                     title:"您填写的学员健身反馈已提交",
                     type:"success",
                     showConfirmButton:false,
                     timer:2000
+                },function() {
+                    $state.go('coachindex', null, { reload: true });
                 });
             })
           
@@ -29,11 +32,14 @@
 
     app.service("coachwriteinfoService",["$http","domain",function ($http,domain) {
         this.insertLearnerInfo=function (coachAdvice,learnerId,scheId,callback) {
+            console.log("你好，我是按钮的事件！");
             $http({
                 method:"POST",
                 url:domain+'api/learner-infos/fitlog',
                 data:{
                     coachAdvice:coachAdvice,
+                    exerciseData:{},
+                    bodyTestData:{},
                     learnerId:learnerId,
                     scheId:scheId
                 }
